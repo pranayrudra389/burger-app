30eca6023bb080e12eca88493275d0a62b0f6d46
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-07-27 22:26:41 -0400

    * Added Build Control cmp. * Outputting Multiple Buld Controls. * Connected State to Build Controls. * Removing Ingredients Safely. * Displaying and updating the burger price.

---
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {
+        label: 'Salad',
+        type: 'salad'
+    }, {
+        label: 'Bacon',
+        type: 'bacon'
+    }, {
+        label: 'Cheese',
+        type: 'cheese'
+    }, {
+        label: 'Meat',
+        type: 'meat'
+    },
+];
+
+const buildControls = props => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                add={() => props.ingredientAdd(ctrl.type)}
+                remove={() => props.ingredientRemove(ctrl.type)}
+                disabled={props.disabledInfo[ctrl.type]}
+            />
+        ))}
+    </div>
+);
+
+export default buildControls;
