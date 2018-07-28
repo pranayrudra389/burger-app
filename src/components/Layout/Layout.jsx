ef07c31ec24b1f0ceb15b4562a5cf3c586a52682
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-07-28 19:13:17 -0400

    Moved Auxiliary file to its own folder and changed paths for other files.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
