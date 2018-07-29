f66c3b8b8cda91d443642b3f6ab986f0b74f0361
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-07-29 19:09:34 -0400

    * Fecthing the Orders from backend and outputting them.

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount = () => {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({
+                    loading: false,
+                    orders: fetchedOrders
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    loading: false
+                });
+                console.error('Error occurred while fetching the orders: ', err)
+            });
+    };
+
     render () {
+        if (this.state.loading) {
+            return <Spinner />
+        }
         return (
             <div>
-               <Order /> 
-               <Order /> 
+               {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        totalPrice={order.totalPrice}
+                    />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
